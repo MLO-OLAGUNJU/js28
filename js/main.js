734a5f4e24ab6c0febfa9416d4e9efafc851fa23
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-23 15:02:23 +0100

    javascript regEx

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 document.getElementById("phoneNum");
 addEventListener("input", (myEvent) => {
   const regex = /^\(?(\d{3})\)?[-. ]?(\d{3})[-. ]?(\d{4})$/g;
+  const input = document.getElementById("phoneNum");
+  const format = document.querySelector(".phoneFormat");
+  const phone = input.value;
+  const found = regex.test(phone);
+  if (!found && phone.lenght) {
+    input.classList.add("invalid");
+    format.classList.add("block");
+  }
 });
